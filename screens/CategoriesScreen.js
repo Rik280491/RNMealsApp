8b6c92c7107c68708dd6d72c19f71bc4b995b3b1
RikBala <rik280491@googlemail.com>
2020-12-02 17:30:33 +0000

    set up stack navigation

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,20 +1,26 @@
-import React from "react"
-import { View, Text, StyleSheet } from "react-native"
+import React from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
 
-const CategoriesScreen = props => {
-    return (
-        <View style={styles.screen}>
-            <Text>The Categories Screen!</Text>
-        </View>
-    )
-}
+const CategoriesScreen = (props) => {
+	return (
+		<View style={styles.screen}>
+			<Text>The Categories Screen!</Text>
+			<Button
+				title="Go to Meals"
+				onPress={() => {
+					props.navigation.navigate({ routeName: "CategoryMeals" });
+				}}
+			/>
+		</View>
+	);
+};
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-    }
-})
+	screen: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+});
 
-export default CategoriesScreen
+export default CategoriesScreen;
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,20 +1,26 @@
-import React from "react"
-import { View, Text, StyleSheet } from "react-native"
+import React from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
 
-const CategoryMealsScreen = props => {
-    return (
-        <View style={styles.screen}>
-            <Text>The Category Meal Screen!</Text>
-        </View>
-    )
-}
+const CategoryMealsScreen = (props) => {
+	return (
+		<View style={styles.screen}>
+			<Text>The Category Meal Screen!</Text>
+			<Button
+				title="Go to Meal Details"
+				onPress={() => {
+					props.navigation.navigate({ routeName: "MealDetail" });
+				}}
+			/>
+		</View>
+	);
+};
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-    }
-})
+	screen: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+});
 
-export default CategoryMealsScreen
+export default CategoryMealsScreen;
